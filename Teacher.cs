8b5ccfc61222ab3efacc76342aee4a562b15073b
Ardiyan29 <ardiyanrizkinugraha@students.amikom.ac.id>
2022-07-08 20:48:26 +0700

    initial commit

---
+++ Teacher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Teacher : Person
+    {
+
+        private string teacherId;
+        private string subject;
+        public string Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+
+        public string TeacherId
+        {
+            get { return teacherId; }
+            set { teacherId = value; }
+        }
+
+
+        public Teacher(string name, int age, string teacherId, string subject) : base(name, age)
+        {
+            this.teacherId = teacherId;
+            this.subject = subject;
+        }
+    }
+}
